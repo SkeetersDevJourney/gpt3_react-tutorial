7765f9cceae0f3b7d77c8c38ba5de0d6bfabe303
SkeetersDevJourney <skeeters.devjourney@gmail.com>
2022-08-13 16:03:30 -0400

    Initialize React Tutorial

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+
+/* allows us to hook our react application 
+to the index.html */
+
+ReactDOM.render(<App />, document.getElementById('root'));
