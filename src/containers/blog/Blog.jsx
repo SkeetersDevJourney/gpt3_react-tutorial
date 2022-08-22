5e7dadce7f8b400fe42f8700a37f9f4e25fe69d0
SkeetersDevJourney <skeeters.devjourney@gmail.com>
2022-08-22 14:48:11 -0400

    last article updates

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -12,7 +12,10 @@ We are blogging about it.</h1>
       </div>
       <div className='gpt3__blog-container'>
         <div className='gpt3__blog-container__groupA'>
-          <Article imgUrl={blog01}/>
+          <Article 
+            imgUrl={blog01}
+            date='Sep 26, 2021' 
+            title='GPT-3 and Open  AI is the future. Let us exlore how it is?'/>
         </div>
         <div className='gpt3__blog-container__groupB'>
           <Article 
